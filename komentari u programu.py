79b2e0f1ea64c9b8e10f2ccb5154a1cb88ea68f1
sarabarac <sbarac471@gmail.com>
2017-06-30 13:52:43 +0200

    Add files via upload

---
+++ komentari u programu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#print ("Ovo je prva linija")
+
+""" ovo je komentar
+ovo je i dalje komentar
+ovo je i dalje i dalje komentar
+"""
+
+
+print("Hello world!")
+print("Ovo je druga linija")
+
