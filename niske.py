eee88e5db538d5ad00defc44ff44f93a4479a3df
sarabarac <sbarac471@gmail.com>
2017-06-30 16:25:03 +0200

    Add files via upload

---
+++ niske.py
@@ -0,0 +1,27 @@
+# -*- coding: utf
+
+print(1458 + 8597 == 10055) 
+
+
+print(5 + 5 < 6 + 6)
+
+print(8 + 4 >= 8 + 5)
+
+print(len("kolač") != len("torta"))
+
+print("kolač" != "torta")
+
+x = "Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+
+x = x.lower()
+
+lista_reči = x.split(" ")
+
+print(lista_reči)
+
+x = "Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+
+list(x)
+
+broj = 5
+print(int(broj) + 10 )
