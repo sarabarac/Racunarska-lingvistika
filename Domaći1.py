3047e98fc01cf0751467a64ec6e96c8413101a3a
sarabarac <sbarac471@gmail.com>
2017-07-01 00:53:41 +0200

    Add files via upload

---
+++ Domaći1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+priča = "Živele u jednoj bari tri žabe. Svađale su se oko toga koja od njih najbolje peva, čiji je glas najlepši. Njihovoj svađi nikad ne bi došao kraj da nisu odlučile da organizuju takmičenje da se vidi ko je najbolji. To je za stanovnike bare i okoline bio veliki događaj. Sve žabe, i velike i male, su došle da svojim prisustvom pomognu odluku žirija. Ali pred sam početak svi shvatiše da nisu izabrali žiri. Mnogo njih je želelo da odlučuju koja od tri žabe najlepše peva i zato je bila takva galama da niko nikog nije čuo šta govori. Roda, patka i zmija čuju kreketanje (a taj zvuk ih je neodoljivo privlačio) i požure da vide šta se događa. Žabe, ne razmišljajući mnogo, zamole rodu, patku i zmiju da budu članovi žirija. One pristanu i takmičenje poče. Tri žabe su naizmenično pevale sve dok nisu iscrple svoj bogati repertoar i dok nisu promukle. Publika je bila oduševljena. Članovi žirija zamoliše za tišinu i rekoše takmičarkama da priđu bliže da im čestitaju i proglase pobednicu. I žabe priđoše umorne i sretne. Možemo zamisliti šta se dogodilo takmičarkama. A i publici iz prvih redova. Te večeri su roda, patka i zmija imale obilnu večeru. Žabama nije bilo do pesme. Celu noć su razmišljale i šaputale. Tako je devojčica po imenu Sofija završila svoju priču. Zamislite sebe na mestu žaba. Da li biste uopšte pevali ? Da li biste dozvolili da u trenutku zaboravite na opreznost i budete pojedeni? Razmislite o tome. Izašla je iz sobe i ostavila sve zamišljene." 
+
+print(priča)
+
+rečenice = priča.split(". ")
+
+print(rečenice)
+
+print(len(rečenice))
