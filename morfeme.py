73eedc104371ab6409381a93c8c5ec34327e99a7
sarabarac <sbarac471@gmail.com>
2017-06-30 17:02:53 +0200

    Add files via upload

---
+++ morfeme.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+niska = "Da li je moguće da sam i dalje niska?!"
+jedan_karakter = niska[0]
+isti_karakter = niska[-38]
+print(jedan_karakter)
+print(isti_karakter)
+
+
+nekoliko_karaktera = niska[0:15]
+nekoliko_karaktera_s_kraja = niska[-15:-2]
+
+
+
+reč1 = "pevač"
+
+reč1_osnova = reč1[0:3]
+print(reč1_osnova)
+
+reč1_sufiks = reč1[-2:5]
+print(reč1_sufiks)
+
+
+
+reč2 = "beogradski"
+
+reč2_osnova = reč2[0:7]
+print(reč2_osnova)
+
+reč2_sufiks = reč2[7:]
