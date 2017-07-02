58df42d0f0c63a0825e5c30491480117484d5acc
sarabarac <sbarac471@gmail.com>
2017-07-02 12:36:04 +0200

    Add files via upload

---
+++ Domaći2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+rečnik = { "sreća":{"sinonimi":[ "radost", "ushićenje" ], "antonimi": [ "tuga", "čemer"]},  "malo" : {"sinonimi": ["sićušno", "malecno"], "antonimi": ["ogromno" , "veliko"]}, "lep": { "sinonimi": ["prijatan", "umilan", "divan"], "antonimi": ["ružan", "neprijatan"]}}
+
+print(rečnik["sreća"]["sinonimi"])
+print(rečnik["sreća"]["antonimi"])
+
+print(rečnik["lep"]["sinonimi"][0]) 
+print(rečnik["lep"]["antonimi"][1])
+
+print(rečnik["malo"]["sinonimi"])
+print(rečnik["malo"]["antonimi"])
+
